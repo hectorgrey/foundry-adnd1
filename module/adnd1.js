7c97b29d6e00850388fb5a5a0646c6fb24bfbc34
hectorgrey <parker321@gmail.com>
2020-08-31 19:11:29 +0100

    changed indentation on core js file

--- module/adnd1.js
+++ module/adnd1.js
@@ -7,43 +7,43 @@ import { ADNDItemSheet } from "./item/item-sheet.js";
 
 Hooks.once('init', async function() {
 
-  game.adnd1 = {
-    ADNDActor,
-    ADNDItem
-  };
+    game.adnd1 = {
+        ADNDActor,
+        ADNDItem
+    };
 
-  /**
-   * Set an initiative formula for the system
-   * @type {String}
-   */
-  CONFIG.Combat.initiative = {
-    formula: "1d6",
-    decimals: 0
-  };
+    /**
+     * Set an initiative formula for the system
+     * @type {String}
+     */
+    CONFIG.Combat.initiative = {
+        formula: "1d6",
+        decimals: 0
+    };
 
-  // Define custom Entity classes
-  CONFIG.Actor.entityClass = ADNDActor;
-  CONFIG.Item.entityClass = ADNDItem;
+    // Define custom Entity classes
+    CONFIG.Actor.entityClass = ADNDActor;
+    CONFIG.Item.entityClass = ADNDItem;
 
-  // Register sheet application classes
-  Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("adnd1", ADNDMonsterSheet, { types: ["monster"], makeDefault: true });
-  Actors.registerSheet("adnd1", ADNDCharacterSheet, { types: ["character"], makeDefault: true });
-  Items.unregisterSheet("core", ItemSheet);
-  Items.registerSheet("adnd1", ADNDItemSheet, { makeDefault: true });
+    // Register sheet application classes
+    Actors.unregisterSheet("core", ActorSheet);
+    Actors.registerSheet("adnd1", ADNDMonsterSheet, { types: ["monster"], makeDefault: true });
+    Actors.registerSheet("adnd1", ADNDCharacterSheet, { types: ["character"], makeDefault: true });
+    Items.unregisterSheet("core", ItemSheet);
+    Items.registerSheet("adnd1", ADNDItemSheet, { makeDefault: true });
 
-  // If you need to add Handlebars helpers, here are a few useful examples:
-  Handlebars.registerHelper('concat', function() {
-    var outStr = '';
-    for (var arg in arguments) {
-      if (typeof arguments[arg] != 'object') {
-        outStr += arguments[arg];
-      }
-    }
-    return outStr;
-  });
+    // If you need to add Handlebars helpers, here are a few useful examples:
+    Handlebars.registerHelper('concat', function() {
+        var outStr = '';
+        for (var arg in arguments) {
+            if (typeof arguments[arg] != 'object') {
+                outStr += arguments[arg];
+            }
+        }
+        return outStr;
+    });
 
-  Handlebars.registerHelper('toLowerCase', function(str) {
-    return str.toLowerCase();
-  });
+    Handlebars.registerHelper('toLowerCase', function(str) {
+        return str.toLowerCase();
+    });
 });
