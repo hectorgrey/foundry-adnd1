1e346360f5c2968b8431c7b8ce4c5d68f57ac56d
hectorgrey <parker321@gmail.com>
2020-09-07 22:19:53 +0100

    removed example handlebars helpers

--- module/adnd1.js
+++ module/adnd1.js
@@ -31,19 +31,4 @@ Hooks.once('init', async function() {
     Actors.registerSheet("adnd1", ADNDCharacterSheet, { types: ["character"], makeDefault: true });
     Items.unregisterSheet("core", ItemSheet);
     Items.registerSheet("adnd1", ADNDItemSheet, { makeDefault: true });
-
-    // If you need to add Handlebars helpers, here are a few useful examples:
-    Handlebars.registerHelper('concat', function() {
-        var outStr = '';
-        for (var arg in arguments) {
-            if (typeof arguments[arg] != 'object') {
-                outStr += arguments[arg];
-            }
-        }
-        return outStr;
-    });
-
-    Handlebars.registerHelper('toLowerCase', function(str) {
-        return str.toLowerCase();
-    });
 });
