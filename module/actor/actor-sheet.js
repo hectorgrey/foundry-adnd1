fe22df4eb07d9247583e4b7a69d345f3f0eef4b2
hectorgrey <parker321@gmail.com>
2020-09-07 22:21:54 +0100

    sorted out warning in actor-sheet class

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -2,6 +2,8 @@
  * Extend the basic ActorSheet with some very simple modifications
  * @extends {ActorSheet}
  */
+import '../../node_modules/foundry-pc-types'
+
 export class ADNDActorSheet extends ActorSheet {
 
     /** @override */
